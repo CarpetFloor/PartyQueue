b91cb784ec8a415bacd602341e0484bb370601a1
CarpetFloor <dylan.bartness@gmail.com>
2025-01-26 01:08:32 -0600

    Start of add song modal

--- client.js
+++ client.js
@@ -1,1 +1,22 @@
 let socket = io();
+let addModal = document.querySelector("#addModal");
+
+let closeModalButton = document.querySelector("#close");
+closeModalButton.addEventListener("click", () => {
+    addModal.style.display = "none";
+})
+
+let searchResultsContainer = document.querySelector("#results");
+let searchButton = document.querySelector("#searchButton");
+searchButton.addEventListener("click", () => {
+    searchResultsContainer.style.display = "flex";
+    searchResultsContainer.style.maxHeight = "100%";
+})
+
+let addSongButton = document.querySelector("#addSong");
+addSongButton.addEventListener("click", () => {
+    searchResultsContainer.style.display = "none";    
+    addModal.style.display = "flex";
+    addModal.style.opacity = "1";
+    searchResultsContainer.classList.toggle("show");
+})
